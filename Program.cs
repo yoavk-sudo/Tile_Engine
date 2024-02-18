f713f1a1e47e8f105d8dda01100b72e4c6f76d89
yoavk-sudo <kenkendler123@gmail.com>
2024-02-18 22:50:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tile_Engine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
