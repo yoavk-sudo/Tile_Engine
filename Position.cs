99d3309a4e6df529b3464448f96f92790713ff18
yoavk-sudo <kenkendler123@gmail.com>
2024-02-19 01:27:09 +0200

    Created and finished struct Position

---
+++ Position.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tile_Engine
+{
+    internal struct Position
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if(obj == null) throw new ArgumentNullException(nameof(obj));
+            if (obj is not Position pos) throw new ArgumentException("object must be struct Position");
+            return X == pos.X && Y == pos.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public static Position operator +(Position posOne, Position posTwo) => new Position(posOne.X + posTwo.X, posOne.Y + posTwo.Y);
+        public static Position operator -(Position posOne, Position posTwo) => new Position(posOne.X - posTwo.X, posOne.Y - posTwo.Y);
+    }
+}
