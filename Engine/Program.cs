cea5ef8f61737e04139c11d49f08ff9933e0acc8
yoavk-sudo <kenkendler123@gmail.com>
2024-03-04 18:57:10 +0200

    Whole project

---
+++ Engine/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Tile_Engine
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Commands.ExecuteCommand(2.ToString());
+            TileMap map = new(3, 5);
+            TileObject obj = new("AAAA");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    map.AddNewObject((TileObject)obj.Clone(), new Position(i, j));
+                }
+            }
+            TileObject obj2 = new("AssA");
+            map.AddNewObject(obj, new Position(0, 0));
+            map.AddNewObject(obj2, new Position(0, 1));
+            Commands.ExecuteCommand("select");
+        }
+    }
+}
+
