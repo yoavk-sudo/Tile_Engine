549aec576a8bef7ff73c7133e0fc81e5c78bc7c3
SummerNova <summern0v4@gmail.com>
2024-03-18 19:08:12 +0200

    basic turn handler

---
+++ Engine/ITurnHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tile_Engine
+{
+    public abstract class AbstractTurnHandler
+    {
+        public event Action NewTurn;
+
+        public void EndTurn();
+    }
+}
