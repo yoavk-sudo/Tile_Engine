82324777ff1aeaa1bc20fee41d7701ac152559fc
SummerNova <summern0v4@gmail.com>
2024-06-18 21:44:28 +0300

    Text Box Exists and works

--- MonoTileGame/TileGame.cs
+++ MonoTileGame/TileGame.cs
@@ -19,6 +19,9 @@ namespace MonoTileGame
         private readonly int _resolutionWidth = 1920;
         private readonly int _resolutionHeight = 1080;
 
+        private TextBox inputBox;
+        private Logger _logger;
+
         // Actual Render Resolution
 
         private int _virtualWidth = 1920;
@@ -34,8 +37,9 @@ namespace MonoTileGame
         private TileMap _tileMap;
         private int TileSize;
 
-        public Action<Tile> MousePressedOnTile = (t) => { return; };
+        public event Action<Tile> MousePressedOnTile = (t) => { return; };
         public event Action<int, int> MousePressedOutsideOfTileMap = (t,s) => { return; };
+        public event Action<string> CommandLineTrigger = (t) => { return; };
 
         public TileGame(TileMap tileMap)
         {
@@ -128,29 +132,128 @@ namespace MonoTileGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            LoadLibrary.Instance.LoadData(new Sprite("inputBackground", "input background"));
+            LoadLibrary.Instance.LoadData(new Sprite("logBackground", "Logger Background"));
+            LoadLibrary.Instance.LoadData(new Sprite("TextCursor", "TextCursor"));
 
+            SpriteFont font = Content.Load<SpriteFont>("TextFont");
 
+            int bufferWidth = 30;
+
+            inputBox = new TextBox(LoadLibrary.Instance.GetTexture("inputBackground"), LoadLibrary.Instance.GetTexture("TextCursor"),
+                                            new Point(_resolutionWidth - TileSize * TileMap.Map.GetLength(0) - 2 * bufferWidth, 50),
+                                            new Point(5, 30), new Vector2(TileMap.Map.GetLength(0) * TileSize + bufferWidth, _resolutionHeight - 100),
+                                            100, true, true, font, 0, Text: "i have starting text");
+            
+            _logger = new Logger((int)inputBox.Position.X, (int)inputBox.Position.Y,inputBox.CellWidth,inputBox.CellHeight,bufferWidth,font,100);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            MouseState inputData = Mouse.GetState();
+            MouseState inputData = OneShotMouseButtons.GetState();
             int updatedX = ((inputData.X - _viewport.X) * _resolutionWidth) / _virtualWidth;
             int updatedY = ((inputData.Y - _viewport.Y) * _resolutionHeight) / _virtualHeight;
-            if (inputData.LeftButton == ButtonState.Pressed)
+            if (OneShotMouseButtons.IsPressed(true)&&OneShotMouseButtons.HasNotBeenPressed(true))
             {
                 if (updatedX > TileSize * TileMap.Map.GetLength(0) || updatedY > TileSize * TileMap.Map.GetLength(1) || updatedX < 0 || updatedY < 0)
                 {
-                    MousePressedOutsideOfTileMap.Invoke(updatedX, updatedY);
+                    if (updatedX < inputBox.Position.X + inputBox.CellWidth && updatedX > inputBox.Position.X && updatedY < inputBox.Position.Y + inputBox.CellHeight && updatedY > inputBox.Position.Y )
+                    {
+                        inputBox.Selected = !inputBox.Selected;
+                        
+                    }
+                    else
+                    {
+                        inputBox.Selected=false;
+                        MousePressedOutsideOfTileMap.Invoke(updatedX, updatedY);
+                    }
                 }
                 else
                 {
                     //TileMap.Map[inputData.X / TileSize, inputData.Y / TileSize].Texture.Tint(new Object());
                     MousePressedOnTile.Invoke(TileMap.Map[updatedX / TileSize, updatedY / TileSize]);
+                    inputBox.Selected = false;
                 }
             }
 
+            if (inputBox.Selected)
+            {
+                HandleInput(gameTime);
+
+            }
+
+            inputBox.Update(gameTime);
+
             base.Update(gameTime);
+        }
+
+        private void HandleInput(GameTime gameTime)
+        {
+            KeyboardState state = OneShotKeyboard.GetState();
+            Keys[] keys = state.GetPressedKeys();
+            string value = String.Empty;
+
+            if (keys.Length > 0)
+            {
+                if (keys.Length > 1) keys[0] = ExtractSingleChar(keys);
+
+                if(state.IsKeyDown(Keys.Back) || state.IsKeyDown(Keys.Delete))
+                {
+                    if (OneShotKeyboard.HasNotBeenPressed(Keys.Delete))
+                    {
+                        if (inputBox.Selected) inputBox.AddMoreText('\b');
+                    }
+
+                    if (OneShotKeyboard.HasNotBeenPressed(Keys.Back))
+                    {
+                        if (inputBox.Selected) inputBox.AddMoreText('\b');
+                    }
+
+                    return;
+                }
+
+                if (inputBox.Selected && ((int)keys[0] >= 48 && (int)keys[0] <= 105) || keys[0] == Keys.RightShift || keys[0] == Keys.LeftShift)
+                {
+                    if (keys[0] == Keys.RightShift || keys[0] == Keys.LeftShift)
+                    {
+                        if (OneShotKeyboard.HasNotBeenPressed(keys[0])) return;
+                    }
+
+                    if (OneShotKeyboard.HasNotBeenPressed(keys[0]))
+                    {
+                        if ((int)keys[0] >= 96 && (int)keys[0] <= 105)
+                        {
+                            value = keys[0].ToString().Substring(keys[0].ToString().Length - 1);
+                            inputBox.AddMoreText(value.ToCharArray()[0]);
+                        }
+                        else
+                            inputBox.AddMoreText((char)keys[0]);
+                    }
+
+                }
+                else if (OneShotKeyboard.HasNotBeenPressed(keys[0]) && keys[0] == Keys.Space) inputBox.AddMoreText((char)keys[0]);
+
+
+            }
+
+            if (OneShotKeyboard.IsPressed(Keys.Enter) && OneShotKeyboard.HasNotBeenPressed(Keys.Enter))
+            {
+                _logger.PushNewText(inputBox.CurrentText);
+                CommandLineTrigger.Invoke(inputBox.CurrentText);
+                inputBox.InsertNewText("");
+            }
+        }
+
+        private Keys ExtractSingleChar(Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                if ((int)key >=48 && (int)key <= 105)
+                {
+                    return key;
+                }
+            }
+            return Keys.None;
         }
 
         protected override void Draw(GameTime gameTime)
@@ -167,9 +270,18 @@ namespace MonoTileGame
 
             HandleGamePieces();
 
+            HandleText();
+
             base.Draw(gameTime);
         }
 
+        private void HandleText()
+        {
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp, transformMatrix: _screenScaleMatrix);
+            inputBox.Render(_spriteBatch);
+            _logger.Render(_spriteBatch);
+            _spriteBatch.End();
+        }
 
         protected void HandleBackground()
         {
